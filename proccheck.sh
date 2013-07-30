f76ca3e1bb4df0b4565125a2ce21a530124c3bd4
ruthealee <leere@tcd.ie>
2013-07-30 10:11:32 +0000

    Added shebang

--- proccheck.sh
+++ proccheck.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 if [ $(grep -m1 "cpu cores" /proc/cpuinfo | awk '{print $4}') -eq $(grep -m1 "siblings" /proc/cpuinfo | awk '{print $3}') ]; then echo "Physical Cores: $(grep -c "processor" /proc/cpuinfo)"; else echo $(($(grep -c "processor" /proc/cpuinfo) / 2 )); fi;
